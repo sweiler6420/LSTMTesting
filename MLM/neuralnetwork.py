eba9ff55038887b0835a361e6b815bf6e056d62e
sweiler6420 <39660360+sweiler6420@users.noreply.github.com>
2022-05-20 19:22:01 -0400

    working properly

--- MLM/neuralnetwork.py
+++ MLM/neuralnetwork.py
@@ -1,3 +1,12 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+"""
+0 = all messages are logged (default behavior)
+1 = INFO messages are not printed
+2 = INFO and WARNING messages are not printed
+3 = INFO, WARNING, and ERROR messages are not printed
+"""
+
 import math
 from typing import List
 
@@ -5,6 +14,7 @@ import pandas_datareader as web
 
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -14,8 +24,12 @@ from keras.layers import Dense, LSTM
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
 
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.FATAL)
+warnings.filterwarnings('error')
 plt.style.use('fivethirtyeight')
+pd.options.mode.chained_assignment = None
 
 class NeuralNetwork():
 
@@ -30,7 +44,6 @@ class NeuralNetwork():
         input_csv {str} -- the path to the csv used to train/test the model.
         """
         self.scaler = MinMaxScaler(feature_range=(0,1))
-        self.scale = None
         self.x_train = []
         self.y_train = []
         self.x_test = []
@@ -38,11 +51,12 @@ class NeuralNetwork():
         self.training_data_len = 0
         self.lookback = 0
         self.test_predictions = None
-        self.y_data = None
+        self.y_dataset = None
         self.y_column_name = None
         self.y_column_num = 0
         self.x_column_num = 0
         self.history = None
+        self.actual_vs_preds = None
 
         # Set variables
         self.save_model = save_model
@@ -80,7 +94,7 @@ class NeuralNetwork():
         #if the layer type is lstm create an lstm layer, same with Dense. Possible to add more options later
         if layer_type == "LSTM" or layer_type == "lstm":
             if input_shape:
-                self.model.add(LSTM(neurons, return_sequences=return_sequence, input_shape=(self.x_train.shape[1],self.x_column_num), dropout=dropout))
+                self.model.add(LSTM(neurons, return_sequences=return_sequence, input_shape=(self.x_train.shape[1], self.x_train.shape[2]), dropout=dropout))
             else:
                 self.model.add(LSTM(neurons, return_sequences=return_sequence, dropout=dropout))
         elif layer_type == "DENSE" or layer_type == "dense":
@@ -98,9 +112,11 @@ class NeuralNetwork():
         # Compile the model before training
         self.model.compile(optimizer='adam', loss='mse', metrics='accuracy')
         # Train the model
-        self.history = self.model.fit(self.x_train, self.y_train, batch_size=batch_size, epochs=epochs)
+        self.history = self.model.fit(self.x_train, self.y_train, validation_data = (self.x_test, self.y_test), batch_size=batch_size, epochs=epochs) #validation_data = (self.x_test, self.y_test),
+        _, self.test_accuracy = self.model.evaluate(self.x_test, self.y_test)
+        self.test_accuracy = round(self.test_accuracy,3)
 
-    def normalize_data(self, y_column_name: List[str], lookback: int = 50, training_percent: float = 0.8, scale: bool = True):
+    def normalize_data(self, y_column_name: List[str], lookback: int = 50, training_percent: float = 0.8):
         """
         Create the dataframes and normalizes the data to the correct, type, size, and breaks them up into x_train, y_train, x_test, y_test arrays
         Arguments:
@@ -111,72 +127,38 @@ class NeuralNetwork():
         training_percent {float} -- value between 0-1 that is used to decide how to split the training and testing up from the one input file
         """
         # Set column names and get the length of the list
-        self.scale = scale
         self.y_column_name = y_column_name
         self.y_column_num = len(y_column_name)
-        # Save the lookback number
+        self.x_column_num = len(self.df)
         self.lookback = lookback
 
-        # Create a new dataframe with only the specified output column
-        if self.y_column_num == 1:
-            self.y_data = self.df.filter([y_column_name[0]])
-        elif self.y_column_num == 2:
-            self.y_data = self.df.filter([[y_column_name[0], y_column_name[1]]])
-        elif self.y_column_num == 3:
-            self.y_data = self.df.filter([[y_column_name[0], y_column_name[1], y_column_name[2]]])
-        else:
-            self.y_data = self.df.filter([[y_column_name[0], y_column_name[1], y_column_name[2], y_column_name[3]]])
-        
-        x_train_dataset = self.df.values
-        y_train_dataset = self.y_data.values
+        self.output_df = self._get_output_df(y_column_name=y_column_name)
+
+        #convert train datasets to numpy arrays
+        x_dataset = self.df.values
+        y_dataset = self.output_df.values
 
         # Get the number of rows to train the model on and round up
-        self.training_data_len = math.ceil(len(x_train_dataset) * training_percent)
+        self.training_data_len = math.ceil(len(x_dataset) * training_percent)
+
         # Scale the data for normalization (advantagious to preprocess the data for input data to a neural network)
-        if scale:
-            x_scaled_data = self.scaler.fit_transform(x_train_dataset)
-            y_scaled_data = self.scaler.fit_transform(y_train_dataset)
-        else:
-            x_scaled_data = x_train_dataset
-            y_scaled_data = y_train_dataset
+        x_scaled_data = self.scaler.fit_transform(x_dataset)
+        y_scaled_data = self.scaler.fit_transform(y_dataset)
 
         # Create the training dataset 
         # Create the scaled training data set
         x_train_data = x_scaled_data[0:self.training_data_len, :]
         y_train_data = y_scaled_data[0:self.training_data_len, :]
 
-        self.x_column_num = len(x_train_data[0])
+        # Transform the train data sets into normalized sets with sequences and the expected future y value
+        self.x_train, self.y_train = self._create_train_arrays(x_train_data=x_train_data, y_train_data=y_train_data, lookback=self.lookback)       
 
-        # lookback variable is used to hold how many intervals of time will be used to predict the next interval
-        for i in range(lookback, len(x_train_data)):
-            self.x_train.append(x_train_data[i-lookback:i, :self.x_column_num])
-            self.y_train.append(y_train_data[i, :self.x_column_num])
-            # if i <= lookback:
-            #     print(self.x_train)
-            #     print(self.y_train)
+        ############################### Create the testing data set#########################
+        # Create a new array containing scaled values from the remaining input csv values
+        x_test_data = x_scaled_data[self.training_data_len - lookback: , :]
 
-        #print(str(len(self.x_train[0])))
-
-        # Convert the x_train and y_train to numpy arrays
-        self.x_train, self.y_train = np.array(self.x_train), np.array(self.y_train)
-        # Reshape the x_train data since it will be 3 dimensional
-        self.x_train = np.reshape(self.x_train, (self.x_train.shape[0], self.x_train.shape[1], self.x_column_num))
-
-        # Create the testing data set
-        # Create a new array containing scaled values from the remaining csv
-        test_data = x_scaled_data[self.training_data_len - lookback: , :]
-        # Create the data sets for x_test and y_test
-        self.y_test = y_train_dataset[self.training_data_len:, :]
-        for i in range(lookback, len(test_data)):
-            self.x_test.append(test_data[i-lookback:i, :self.x_column_num])
-
-        # Convert these to numpy arrays
-        self.x_test = np.array(self.x_test)
-
-        # Reshape the x_test to 3 dimensional
-        self.x_test= np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[1], self.x_column_num))
-        # print(self.x_test)
-        # print(self.y_test)
+        # Create the data sets for x_test and y_test, convert them to numpy array and reshape them (same logic as _parse_sequence_array but for output)
+        self.x_test, self.y_test = self._create_test_arrays(x_test_data=x_test_data, y_test_data=y_dataset, lookback=self.lookback)
 
     def prediction_test(self):
         """ 
@@ -185,8 +167,7 @@ class NeuralNetwork():
         # Get the models predicted price values
         self.test_predictions = self.model.predict(self.x_test)
         # Inverse scale the output predictions
-        if self.scale:
-            self.test_predictions = self.scaler.inverse_transform(self.test_predictions)
+        self.test_predictions = self.scaler.inverse_transform(self.test_predictions)
         return self.test_predictions
 
     def get_rmse(self) -> float:
@@ -208,11 +189,26 @@ class NeuralNetwork():
         h1 = self.history.history
         plt.figure(figsize=(20,6))
         plt.subplot(1,3,1)
-        ax = sns.lineplot(y=h1['val_acc'], x = range(len(h1['val_acc'])),label="val_acc",palette="binary")
+        ax = sns.lineplot(y=h1['val_accuracy'], x = range(len(h1['val_accuracy'])),label="val_acc",palette="binary")
         ax.set(xlabel='epochs', ylabel="val acc")
-        ax = sns.lineplot(y=h1['acc'], x = range(len(h1['val_acc'])),label="acc",palette="flare")
+        ax = sns.lineplot(y=h1['accuracy'], x = range(len(h1['val_accuracy'])),label="acc",palette="flare")
         ax.set(xlabel='epochs', ylabel="acc")
-        plt.title('Epochs vs Accuracy (Acc-'+str(self.test_accuracy)+')')
+        plt.title('Epochs vs Accuracy (Acc = '+str(self.test_accuracy)+')')
+
+        plt.subplot(1,3,2)
+        ax = sns.lineplot(x=h1['accuracy'], y = h1['val_accuracy'],label="acc vs val_acc",palette="binary",color="green",sort=False)
+        ax.set(xlabel='acc', ylabel="val acc")
+        plt.legend()
+        plt.title('Validate vs Train Accuracy (Acc = '+str(self.test_accuracy)+')')
+
+        plt.subplot(1,3,3)
+        pdtmp1 = abs(pd.DataFrame(h1['loss'])-pd.DataFrame(h1['val_loss']))
+        pdtmp1.fillna(0,inplace=True)
+        ax = sns.lineplot(y=pdtmp1[0],x=range(0,len(h1['val_loss']),1),label="Loss Convergence",color="red")
+        ax.set(xlabel='epochs',ylabel='abs(val loss - loss)')
+        plt.legend()
+        plt.title('Loss (Acc = '+str(self.test_accuracy)+')')
+        plt.show()
 
     def plot_predictions_test(self):
         """ 
@@ -220,17 +216,19 @@ class NeuralNetwork():
         """
         # Visualize the two datasets
         #print(self.y_data)
-        train = self.y_data[:self.training_data_len]
-        valid = self.y_data[self.training_data_len:]
-        valid['Predictions'] = self.test_predictions
+        train = self.output_df[:self.training_data_len]
+        actual_vs_preds = self.output_df[self.training_data_len:]
+
+        actual_vs_preds['Predictions'] = self.test_predictions
+
+        self.actual_vs_preds = actual_vs_preds
 
         plt.figure(figsize=(16,8))
         plt.title('Model')
         plt.xlabel('Date', fontsize=18)
         plt.ylabel('Close Price USD $', fontsize=18)
-        plt.gcf().autofmt_xdate()
         plt.plot(train[self.y_column_name[0]])
-        plt.plot(valid[[self.y_column_name[0], 'Predictions']])
+        plt.plot(actual_vs_preds[[self.y_column_name[0], 'Predictions']])
         plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
         plt.show()
 
@@ -244,3 +242,53 @@ class NeuralNetwork():
         plt.xlabel('Time', fontsize=18)
         plt.ylabel('Close Price USD $', fontsize=18)
         plt.show()
+
+    def get_actual_vs_preds(self):
+        return self.actual_vs_preds
+
+    def _create_train_arrays(self, x_train_data, y_train_data, lookback: int):
+        # lookback variable is used to hold how many intervals of time will be used to predict the next interval
+        x_train = []
+        y_train = []
+
+        for i in range(lookback, len(x_train_data)):
+            x_train.append(x_train_data[i-lookback:i, :self.x_column_num])
+            y_train.append(y_train_data[i, :self.x_column_num])
+            # if i <= lookback:
+            #     print(x_train)
+            #     print(y_train)
+
+        # Convert the arrays to numpy arrays
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        # Reshape the x_train data since it will be 3 dimensional (3rd dimension is key for multifeature inputs)
+        # x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], self.x_column_num))
+        return x_train, y_train
+
+    def _create_test_arrays(self, x_test_data, y_test_data, lookback: int):
+        # lookback variable is used to hold how many intervals of time will be used to predict the next interval
+        x_test = []
+        y_test = []
+
+        y_test = y_test_data[self.training_data_len:, :]
+        for i in range(lookback, len(x_test_data)):
+            x_test.append(x_test_data[i-lookback:i, :self.x_column_num])
+        
+        # Convert the arrays to numpy arrays
+        x_test, y_test = np.array(x_test), np.array(y_test)
+
+        return x_test, y_test
+
+    def _get_output_df(self, y_column_name: List[str]):
+        # lookback variable is used to hold how many intervals of time will be used to predict the next interval
+        # Create a new dataframe with only the specified output column
+        if self.y_column_num == 1:
+            output_df = self.df.filter([y_column_name[0]])
+        elif self.y_column_num == 2:
+            output_df = self.df.filter([[y_column_name[0], y_column_name[1]]])
+        elif self.y_column_num == 3:
+            output_df = self.df.filter([[y_column_name[0], y_column_name[1], y_column_name[2]]])
+        else:
+            output_df = self.df.filter([[y_column_name[0], y_column_name[1], y_column_name[2], y_column_name[3]]])
+        
+        return output_df
--- nntest.py
+++ nntest.py
@@ -1,3 +1,4 @@
+from operator import ne
 from MLM.neuralnetwork import NeuralNetwork
 
 neural_network = NeuralNetwork(
@@ -8,22 +9,25 @@ neural_network = NeuralNetwork(
 
 y_columns = ["Close"]
 
-neural_network.normalize_data(y_column_name=y_columns, lookback=60, training_percent=0.8)
+neural_network.normalize_data(y_column_name=y_columns, lookback=10, training_percent=0.8)
 
-neural_network.add_layer(layer_type='lstm', neurons=100, return_sequence=True, input_shape=True, dropout=0.1)
-neural_network.add_layer(layer_type='lstm', neurons=50, return_sequence=False, dropout=0.1)
-neural_network.add_layer(layer_type='dense', neurons=25)
+neural_network.add_layer(layer_type='lstm', neurons=10, return_sequence=True, input_shape=True, dropout=0.1)
+neural_network.add_layer(layer_type='lstm', neurons=10, return_sequence=False, dropout=0.1)
+neural_network.add_layer(layer_type='dense', neurons=5)
 neural_network.add_layer(layer_type='dense', neurons=1)
 
-neural_network.train_model(batch_size=1, epochs=30)
+neural_network.train_model(batch_size=1, epochs=5)
 
 predictions = neural_network.prediction_test()
-
-print(predictions)
 
 rmse = neural_network.get_rmse()
 print(rmse)
 
-neural_network.plot_loss()
+neural_network.plot_fit()
 
 neural_network.plot_predictions_test()
+
+actual_vs_preds = neural_network.get_actual_vs_preds()
+print(actual_vs_preds)
+
+neural_network.plot_loss()
